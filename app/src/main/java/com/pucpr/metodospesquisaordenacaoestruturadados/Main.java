ccd46d719ee8007c346f73bf6b59b71c1fef5f05
Lucas Vidor Migotto <lvmigotto1@gmail.com>
2025-05-10 18:29:09 +0000

    feat(ui): create UserInterface to handle interactions
    Refact Main class to use UserInterface class

--- app/src/main/java/com/pucpr/metodospesquisaordenacaoestruturadados/Main.java
+++ app/src/main/java/com/pucpr/metodospesquisaordenacaoestruturadados/Main.java
@@ -1,34 +1,10 @@
 package com.pucpr.metodospesquisaordenacaoestruturadados;
 
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-
-import com.google.gson.JsonIOException;
-import com.google.gson.JsonSyntaxException;
-import com.pucpr.metodospesquisaordenacaoestruturadados.models.Book;
-import com.pucpr.metodospesquisaordenacaoestruturadados.models.BooksGraph;
-import com.pucpr.metodospesquisaordenacaoestruturadados.models.Relation;
-import com.pucpr.metodospesquisaordenacaoestruturadados.utils.DataReader;
+import com.pucpr.metodospesquisaordenacaoestruturadados.ui.UserInterface;
 
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<Book> books = new ArrayList<>();
-        ArrayList<Relation> relations = new ArrayList<>();
-
-        try {
-            books = DataReader.loadBooks();
-            relations = DataReader.loadRelations();
-        } catch (JsonSyntaxException | JsonIOException ex) {
-            System.out.println("Error during data loading");
-            System.exit(1);
-        } catch (FileNotFoundException ex) {
-            System.exit(1);
-        }
-
-        BooksGraph graph = new BooksGraph(books, relations);
-
-        System.out.println(graph);
-
+        System.exit(new UserInterface().runApp());
     }
 }
